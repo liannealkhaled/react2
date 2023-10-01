458015a7b12cc7165ada506af5fae76c9bb0466c
lianne <liannealq@hotmail.com>
2023-10-01 20:28:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {products} from "./products.js"
+
 function App() {
+  
+  const productslist = products.map ( (product)=>{
+    return (
+      <div className='design1 '>
+        <img  className= "imgdesign"src ={product.img}/>
+        <h5> {product.order}</h5>
+       <h5> {product.price}</h5> 
+      </div>
+    )
+  } )
   return (
-    <div className="App">
-      <h1> Book store</h1>
-      <h3> the best book store </h3>
-      <img src='https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books_23-2149334862.jpg' />  
+    <div className="App" >
+      <h1> TEA SHOP</h1>
+      <h3> The finest tea taste is here </h3>
+      <img className="imgdesigntitle"src='https://media.gettyimages.com/id/182418478/photo/teabag-in-hot-water.jpg?s=612x612&w=gi&k=20&c=A5cvYWF_QCz5bqZOl_K5fryDBaiIUXiNPT6fkgPDsnY=' />  
+   <div className='productcard'>
+   {productslist} 
     </div>
+    </div>
+
   );
+
+
 }
 
 export default App;
---
+++ src/products.js
@@ -0,0 +1,14 @@
+
+
+const products = [
+    {order: "Black Tea", 
+    price:"1KD", 
+img: "https://media.istockphoto.com/id/177555555/photo/black-tea-in-glass-cup.jpg?s=612x612&w=0&k=20&c=2Flt5yTypvLnfe0ZLvA6dWI3gd2RnNpTMKUBQ1gAAgM="}, 
+
+{order: "Green Tea", 
+    price:"1.500KD", 
+img: "https://images.unsplash.com/photo-1627435601361-ec25f5b1d0e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW4lMjB0ZWF8ZW58MHx8MHx8fDA%3D&w=1000&q=80" }
+
+]
+
+export {products}
