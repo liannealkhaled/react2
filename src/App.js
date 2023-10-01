f004ec31ff21794a88e874556bf6970f3163e6c6
lianne <liannealq@hotmail.com>
2023-10-01 18:57:34 +0300

    hi

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Book store</h1>
+      <h3> the best book store </h3>
+      <img src='https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books_23-2149334862.jpg' />  
     </div>
   );
 }
