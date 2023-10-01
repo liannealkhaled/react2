ae3e49983d981986ff9daaaa6dc1408d3b3ed6db
lianne <liannealq@hotmail.com>
2023-10-01 20:38:22 +0300

    updatingList

--- src/products.js
+++ src/products.js
@@ -8,7 +8,12 @@ img: "https://media.istockphoto.com/id/177555555/photo/black-tea-in-glass-cup.jp
 {order: "Green Tea", 
     price:"1.500KD", 
 img: "https://images.unsplash.com/photo-1627435601361-ec25f5b1d0e5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW4lMjB0ZWF8ZW58MHx8MHx8fDA%3D&w=1000&q=80" }
-
+,
+{
+    order: "Camomile Tea",
+    price: "1.750KD", 
+    img: "https://sleepopolis.com/wp-content/uploads/2023/02/iStock-1277086333-2.jpg"
+}
 ]
 
 export {products}
